e22843c6d8e8da3ae95f8824116e65847d8e8109
bujosa <davidbujosa@gmail.com>
2021-06-18 22:34:51 -0400

    Create a page with file

--- 001/string_to_html/file/main.go
+++ 001/string_to_html/file/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+
+func main() {
+	name :="David Bujosa"
+
+	str := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Hello world!</title>
+	</head>
+	<body>
+	<h1>`+ name +` </h1>
+	</body>
+	</html?
+	`
+
+	nf, err := os.Create("index.html")
+	if err != nil{
+		log.Fatal("error creating file", err)
+	}
+	defer nf.Close()
+
+	io.Copy(nf, strings.NewReader(str))
+}
