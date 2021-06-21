9df4fc00cb845d76817e970bfbcee0b9e2b7763b
bujosa <davidbujosa@gmail.com>
2021-06-20 21:46:03 -0400

    Added file main

---
+++ 001/parse_execute/file/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	nf, err
+
+	err = tpl.Execute(os.Stdout,nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
