ba7fecc9bbdbd2e05e827d30557a658fedaa60fc
bujosa <davidbujosa@gmail.com>
2021-06-29 23:35:11 -0400

    Fixed main

--- 003/handler/main.go
+++ 003/handler/main.go
@@ -8,7 +8,7 @@ import (
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Any code you want in his func")
+	fmt.Println("Any code you want in this func")
 }
 
 func main() {
