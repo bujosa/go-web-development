f18eb9726f81988aefbb9ac64ad01717bd43f69c
bujosa <davidbujosa@gmail.com>
2021-07-07 01:33:02 -0400

    Fixed reponse writter

--- 3. undestanding net http package/ResponseWriter/main.go
+++ 3. undestanding net http package/ResponseWriter/main.go
@@ -8,7 +8,7 @@ import (
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Mcleod-Key", "this is from mcleod")
+	w.Header().Set("Bujosa-Key", "this is from bujosa")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintln(w, "<h1>Any code you want in this func</h1>")
 }
