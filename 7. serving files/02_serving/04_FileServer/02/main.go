9a595e7fc5c6a8a1de2f9f78348ea46ad27c62e3
bujosa <davidbujosa@gmail.com>
2021-07-13 22:52:50 -0400

    Fixed serving files

--- 7. serving files/02_serving/04_FileServer/02/main.go
+++ 7. serving files/02_serving/04_FileServer/02/main.go
@@ -6,18 +6,12 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", dog)
+	http.HandleFunc("/", lion)
 	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
-func dog(w http.ResponseWriter, req *http.Request) {
+func lion(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	io.WriteString(w, `<img src="/resources/toby.jpg">`)
 }
-
-/*
-
-./assets/toby.jpg
-
-*/
