d3269258fad373062d0c846232c66a3253ace2f3
bujosa <davidbujosa@gmail.com>
2021-07-20 04:37:29 -0400

    Eliminated add new version

--- 8. Not found handlers/main.go
+++ 8. Not found handlers/main.go
@@ -12,6 +12,10 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
 	fmt.Println(req.URL.Path)
 	fmt.Fprintln(w, "go look at your terminal")
 }
